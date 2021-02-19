af4c204a8e08a89164dc84dd2342251832906ff6
kaili zhang <1261037560@qq.com>
2021-02-19 23:01:17 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,184 @@
 <template>
-  <div>888</div>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <el-container>
+    <el-aside>
+      <el-menu :default-openeds="['1']">
+        <el-menu-item index="1">邮件模板设置</el-menu-item>
+        <el-menu-item index="1">员工信息上传</el-menu-item>
+        <!-- <el-menu-item index="3">选项3</el-menu-item> -->
+      </el-menu>
+    </el-aside>
+    <el-main>
+      <!-- email template -->
+      <div class="temp_container" v-for="(item, index) in templateList" :key="index" @click="editTemplate(item)">
+        {{item.name}}
+      </div>
+      <!-- excel上传 -->
+      <el-upload
+        class="upload-demo"
+        action="http://carry666.com:3000/uploadExcel"
+        :on-success="handleExcelSuccess"
+      >
+        <el-button size="small" type="primary">+员工生日信息</el-button>
+      </el-upload>
+      <el-upload
+        class="upload-demo"
+        action="http://carry666.com:3000/uploadExcel"
+        :on-success="handleExcelSuccess"
+      >
+        <el-button size="small" type="primary">+员工司龄信息</el-button>
+      </el-upload>
+      <el-button size="small" type="primary" @click="testSendMail">模拟发邮件</el-button>
+    </el-main>
+    <el-dialog
+      title="编辑模板"
+      v-model="dialogVisible"
+      width="80%"
+      :before-close="handleClose">
+      <el-row :gutter="20">
+        <!-- 上传背景图片，设置用户名位置和内容 -->
+        <el-col :span="12">
+          <el-form ref="form" :model="editTemp" label-width="100px">
+            <input type="file" id="file" @change="handleAvatarSuccess">
+            <!-- <el-upload
+              class="upload-demo"
+              action="http://carry666.com:3000/uploadImg"
+              :on-success="handleAvatarSuccess"
+            >
+              <el-button size="small" type="primary">点击上传背景图片</el-button>
+            </el-upload> -->
+            <el-form-item label="收件人位置">
+              <el-input type="number" v-model="editTemp.nameLeft"></el-input>
+              <el-input type="number" v-model="editTemp.nameTop"></el-input>
+            </el-form-item>
+            <el-form-item label="生日数量位置">
+              <el-input type="number" v-model="editTemp.nameLeft"></el-input>
+              <el-input type="number" v-model="editTemp.nameTop"></el-input>
+            </el-form-item>
+          </el-form>
+        </el-col>
+        <!-- 预览邮件 -->
+        <el-col :span="12">
+          <div v-html="editTemp.htmlStr"></div>
+          <div id="email" :style="{position: 'relative',backgroundSize: '100% auto', backgroundRepeat: 'no-repeat',backgroundImage:'url(' + editTemp.imageUrl+')', minHeight: '100%', width: 'auto'}">
+            <p :style="{position: 'absolute', color: '#fff', left: editTemp.nameLeft + '%', top: editTemp.nameTop + '%'}">收件人名称</p>
+            <p :style="{position: 'absolute', color: '#fff', left: editTemp.ageLeft + '%', top: editTemp.ageTop + '%'}">生日数量</p>
+          </div>
+        </el-col>
+      </el-row>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="setTemplate">确 定</el-button>
+        </span>
+      </template>
+    </el-dialog>
+  </el-container>
 </template>
 
 <script>
+import { ref } from "vue";
 // import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import { ElMessage } from 'element-plus'
+// import FileReader from 'filereader'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
+  },
+  setup() {
+    // 解析excel文件
+    const handleExcelSuccess = () => {
+      // 员工信息存储
+    }
+    // 设置邮件模板
+    let dialogVisible = ref(false)
+    let editTemp = ref({})
+    const editTemplate = (item) => {
+      if (!item.id) {
+        return
+      }
+      dialogVisible.value = !dialogVisible.value
+      editTemp.value = item
+    }
+    const setTemplate = () => {
+      dialogVisible.value = false
+      // 设置模板字符串
+      editTemp.value.htmlStr = document.getElementById('email').innerHTML;
+      // 发送修改模板的请求
+      axios.post('http://carry666.com:3000/saveTemplate', editTemp.value)
+        .then(() => {
+          ElMessage.success({
+            message: '模板设置成功',
+            type: 'success'
+          });
+          // 重新获取模板
+          getTemplateList()
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+      editTemp.value = {}
+    }
+    // 上传背景图片成功
+    const handleAvatarSuccess = () => {
+      let reads= new FileReader();
+      let f=document.getElementById('file').files[0];
+      reads.readAsDataURL(f);
+      reads.onload=function () {
+        editTemp.value.imageUrl = reads.result
+      };
+    }
+    let templateList = ref([])
+    // 获取模板历史
+    const getTemplateList = function() {
+      axios.get('http://carry666.com:3000/getTemplateList')
+        .then(resp => {
+          templateList.value = resp.data.list
+        })
+        .catch(e => {
+          console.log(e)
+        })
+    }
+    getTemplateList()
+    // 获取当前用户身份，通讯录信息
+    // 发送邮件
+    const testSendMail = () => {
+      axios.get('http://carry666.com:3000/send')
+        .then(() => {
+          ElMessage.success({
+            message: '模拟邮件发送成功，请到测试邮箱查看',
+            type: 'success'
+          });
+        })
+        .catch(e => {
+          console.log(e)
+        })
+    }
+    return {
+      dialogVisible,
+      editTemp,
+      editTemplate,
+      setTemplate,
+      templateList,
+      handleAvatarSuccess,
+      handleExcelSuccess,
+      testSendMail,
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.temp_container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  border: 1px solid #333;
+  margin-bottom: 20px;
 }
 </style>
---
+++ src/server.js
@@ -0,0 +1,9 @@
+// import axios from 'axios';
+
+// const get = function(url, opt) {
+//     return axios.get(url, opt)
+// }
+
+// const login = get('https://open.work.weixin.qq.com/wwopen/sso/qrConnect', opt)
+
+// export {login}
